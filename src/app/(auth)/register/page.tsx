30baff920ef2e799abefc22ed62066193e17937b
HandikaDiaz <handikaalexandriadiaz@gmail.com>
2025-05-03 21:32:58 +0700

    fix bug

--- src/app/(auth)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import AuthForm from "@/components/AuthForm";
-
-export default function Login() {
-    return (
-        <div className="min-h-screen bg-gray-900 flex items-center justify-center">
-            <AuthForm type="login" />
-        </div>
-    );
-}
